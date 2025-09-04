1ce6fea59e34ee7fbc73ea4b6a37930f402fb414
Marianne Dillard <marianne@harperdb.io>
2025-09-04 12:24:57 -0400

    update for additional regex testing

--- test/common.js
+++ test/common.js
@@ -1,221 +1,187 @@
-import 'dotenv/config'
-import assert from "node:assert"
+import 'dotenv/config';
+import assert from 'node:assert';
 
-const HOST = process.env.HOST
-const PORT = process.env.PORT || 443
-const OPPORT = process.env.OPPORT || 9925
-const SCHEME = process.env.SCHEME
-const AUTH = process.env.AUTH
-const USERNAME = process.env.USERNAME
-const PASSWORD = process.env.PASSWORD
+const HOST = process.env.HOST;
+const PORT = process.env.PORT || 443;
+const SCHEME = process.env.SCHEME;
+const AUTH = process.env.AUTH;
+const USERNAME = process.env.USERNAME;
+const PASSWORD = process.env.PASSWORD;
 const TOKEN = Buffer.from(`${USERNAME}:${PASSWORD}`).toString('base64');
-const REST_HOST = PORT == 443 ? HOST : `${HOST}:${PORT}`
-const OP_HOST = `${HOST}:${OPPORT}`
-
+const REST_HOST = PORT == 443 ? HOST : `${HOST}:${PORT}`;
 
 const fetchWrapper = async (url, options) => {
-  if (AUTH === "true") {
-    options.headers ??= {}
-    options.headers.Authorization = `Basic ${TOKEN}`
-  }
+	if (AUTH === 'true') {
+		options.headers ??= {};
+		options.headers.Authorization = `Basic ${TOKEN}`;
+	}
 
-  return await fetch(url, options)
-}
+	return await fetch(url, options);
+};
 
 const clearTable = async (table) => {
-  const url = `${SCHEME}://${REST_HOST}/${table}/`
+	const url = `${SCHEME}://${REST_HOST}/${table}/`;
 
-  try {
-    const options = {
-      method: "DELETE",
-    }
-    const resp = await fetchWrapper(url, options)
-    return true
-  }
-  catch (e) {
-    console.log(e)
-    return false
-  }
-}
-
-const getItemCount_44 = async (table) => {
-  const url = `${SCHEME}://${REST_HOST}/${table}`
-
-  try {
-    const options = {
-      method: "GET",
-    }
-    const resp = await fetchWrapper(url, options)
-    const data = await resp.json();
-    console.log(data)
-    return data.recordCount
-  }
-  catch (e) {
-    console.log(e)
-    return -1
-  }
-}
-
-const getItemCount_45 = async (table) => {
-  const url = `${SCHEME}://${REST_HOST}/${table}?select(id)`
-
-  try {
-    const options = {
-      method: "GET",
-    }
-
-    const resp = await fetchWrapper(url, options)
-    const data = await resp.json();
-
-    return data.length;
-  }
-  catch (e) {
-    console.log(e)
-    return -1
-  }
-}
-
+	try {
+		const options = {
+			method: 'DELETE',
+		};
+		const resp = await fetchWrapper(url, options);
+		return true;
+	} catch (e) {
+		console.log(e);
+		return false;
+	}
+};
 
 const getItemCount = async (table) => {
-  return await getItemCount_45(table)
-}
+	const url = `${SCHEME}://${REST_HOST}/${table}?select(id)`;
+
+	try {
+		const options = {
+			method: 'GET',
+		};
+
+		const resp = await fetchWrapper(url, options);
+		const data = await resp.json();
+
+		return data.length;
+	} catch (e) {
+		console.log(e);
+		return -1;
+	}
+};
 
 const defaultOptions = {
-  host: "",
-  version: -1,
-  t: -1,
-  expectNotFound: false,
-  useHeader: false,
-  si: -1
-}
+	host: '',
+	version: -1,
+	t: -1,
+	expectNotFound: false,
+	useHeader: false,
+	si: -1,
+};
 
 const checkRedirect = async (path, redirect, options) => {
+	options = { ...defaultOptions, ...options };
 
-  options = { ...defaultOptions, ...options }
+	var url = `${SCHEME}://${REST_HOST}/checkredirect`;
+	var qs = [];
 
-  var url = `${SCHEME}://${REST_HOST}/checkredirect`
-  var qs = []
+	const fetchOptions = { method: 'GET' };
+	if (!options.useHeader) {
+		if (path.includes('?')) {
+			const qString = path.split('?')[1];
+			fetchOptions.headers = { 'x-query-string': qString };
+			path = path.split('?')[0];
+		}
+		qs.push(`path=${path}`);
+	} else {
+		fetchOptions.headers = { Path: path };
+	}
 
-  if (!options.useHeader) {
-    qs.push(`path=${path}`)
-  }
-  if (options.host?.length > 0) {
-    qs.push(`h=${options.host}`)
-  }
-  if (options.version != -1) {
-    qs.push(`v=${options.version}`)
-  }
-  if (options.t != -1) {
-    qs.push(`t=${options.t}`)
-  }
-  if (options.si != -1) {
-    qs.push(`si=${options.si}`)
-  }
+	if (options.host?.length > 0) {
+		qs.push(`h=${options.host}`);
+	}
 
-  if (qs.length > 0) {
-    url += '?' + qs.join('&')
-  }
+	if (options.ho != -1) {
+		qs.push(`ho=${options.ho}`);
+	}
 
-  console.log(url)
+	if (options.version != -1) {
+		qs.push(`v=${options.version}`);
+	}
 
+	if (options.t != -1) {
+		qs.push(`t=${options.t}`);
+	}
 
+	if (qs.length > 0) {
+		url += '?' + qs.join('&');
+	}
 
-  const fetchOptions = { method: "GET" }
-  if (options.useHeader) {
-    fetchOptions.headers = { Path: path }
-  }
+	const resp = await fetchWrapper(url, fetchOptions);
+	const data = await resp.json();
 
-  const resp = await fetchWrapper(url, fetchOptions)
-  const data = await resp.json()
+	if (options.expectNotFound) {
+		assert.equal(resp.status, 404);
+		return;
+	}
 
-
-  //console.log( data )
-
-
-
-  if (options.expectNotFound) {
-    assert.equal(resp.status, 404)
-    return
-  }
-
-  assert.equal(resp.status, 200)
-  assert.equal(data.redirectURL, redirect)
-  assert.equal(data.statusCode, 301)
-
-  return true
-}
+	assert.equal(resp.status, 200);
+	assert.equal(data.redirectURL, redirect);
+	assert.equal(data.statusCode, 301);
+	return true;
+};
 
 const getActiveVersion = async () => {
-  const url = `${SCHEME}://${REST_HOST}/version/`
+	const url = `${SCHEME}://${REST_HOST}/version/`;
 
-  try {
-    const options = {
-      method: "GET",
-    }
-    const resp = await fetchWrapper(url, options)
+	try {
+		const options = {
+			method: 'GET',
+		};
+		const resp = await fetchWrapper(url, options);
 
-    const json = await resp.json();
+		const json = await resp.json();
 
-    return json[0].activeVersion
-
-  }
-  catch (e) {
-    return -1
-  }
-}
+		return json[0].activeVersion;
+	} catch (e) {
+		return -1;
+	}
+};
 
 const setActiveVersion = async (version) => {
+	clearTable('version');
 
-  clearTable('version')
+	const url = `${SCHEME}://${REST_HOST}/version/`;
 
-  const url = `${SCHEME}://${REST_HOST}/version/`
+	const body = JSON.stringify({ activeVersion: version });
 
-  const body = JSON.stringify({ 'activeVersion': version })
+	try {
+		const options = {
+			method: 'POST',
+			headers: {
+				'Content-type': 'application/json',
+			},
+			body: body,
+		};
+		const resp = await fetchWrapper(url, options);
+	} catch (e) {
+		console.log(e);
+		return false;
+	}
 
-
-  try {
-    const options = {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json"
-      },
-      body: body
-    }
-    const resp = await fetchWrapper(url, options)
-  }
-  catch (e) {
-    console.log(e)
-    return false
-  }
-
-  return true
-}
-
+	return true;
+};
 
 const addToHostTable = async (host, hostOnly) => {
+	const url = `${SCHEME}://${REST_HOST}/hosts/`;
 
-  const url = `${SCHEME}://${REST_HOST}/hosts/`
+	const body = JSON.stringify({ host: host, hostOnly: hostOnly });
 
-  const body = JSON.stringify({ host: host, hostOnly: hostOnly })
+	try {
+		const options = {
+			method: 'POST',
+			headers: {
+				'Content-type': 'application/json',
+			},
+			body: body,
+		};
+		const resp = await fetchWrapper(url, options);
+	} catch (e) {
+		console.log(e);
+		return false;
+	}
 
-  try {
-    const options = {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json"
-      },
-      body: body
-    }
-    const resp = await fetchWrapper(url, options)
-  }
-  catch (e) {
-    console.log(e)
-    return false
-  }
+	return true;
+};
 
-  return true;
-}
-
-
-export { clearTable, getItemCount, fetchWrapper, checkRedirect, getActiveVersion, setActiveVersion, addToHostTable }
-
+export {
+	clearTable,
+	getItemCount,
+	fetchWrapper,
+	checkRedirect,
+	getActiveVersion,
+	setActiveVersion,
+	addToHostTable,
+};
